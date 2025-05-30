d109ab5be177c8c24d63b292ffe5e9d5e84abceb
mxlib <mxlib@outlook.com>
2025-05-30 10:03:14 +0800

    Create baidu.js

---
+++ baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d7da44b0d542b877768bae3fce3072aa";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
